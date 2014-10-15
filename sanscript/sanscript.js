4ead5ec6d00aea727a6f71ca884686908df260f6
Shreeshrii <shreeshrii@gmail.com>
2014-10-15 22:35:17 +0530

    fix indenting

--- sanscript/sanscript.js
+++ sanscript/sanscript.js
@@ -47,7 +47,7 @@
             symbols: '০ ১ ২ ৩ ৪ ৫ ৬ ৭ ৮ ৯ ॐ ঽ । ॥'.split(' '),
             other: '    ড ঢ  য '.split(' '),
             accent: ["", ""],
-			candra: [''],
+            candra: [''],
             combo_accent: ["", "", "", ""]
         },
 
@@ -113,10 +113,10 @@
             consonants: 'ક ખ ગ ઘ ઙ ચ છ જ ઝ ઞ ટ ઠ ડ ઢ ણ ત થ દ ધ ન પ ફ બ ભ મ ય ર લ વ શ ષ સ હ ળ ક્ષ જ્ઞ'.split(' '),
             symbols: '૦ ૧ ૨ ૩ ૪ ૫ ૬ ૭ ૮ ૯ ૐ ઽ . ..'.split(' '),
             candra: ['ૅ'],
-			skip: [''],
+            skip: [''],
             accent: ["", ""],
             combo_accent: ["", "", "", ""],
-		    other: 'ક ખ ગ જ ડ ઢ ફ ય ર'.split(' ')
+        other: 'ક ખ ગ જ ડ ઢ ફ ય ર'.split(' ')
         },
 
         /* Gurmukhi
@@ -132,7 +132,7 @@
             symbols: '੦ ੧ ੨ ੩ ੪ ੫ ੬ ੭ ੮ ੯ ॐ ऽ । ॥'.split(' '),
             other: ' ਖ ਗ ਜ ਡ  ਫ  '.split(' '),
             accent: ["", ""],
-			candra: [''],
+            candra: [''],
             combo_accent: ["", "", "", ""]
         },
 
@@ -149,7 +149,7 @@
             symbols: '೦ ೧ ೨ ೩ ೪ ೫ ೬ ೭ ೮ ೯ ಓಂ ಽ । ॥'.split(' '),
             other: '      ಫ  ಱ'.split(' '),
             accent: ["", ""],
-			candra: [''],
+            candra: [''],
             combo_accent: ["", "", "", ""]
         },
 
@@ -166,7 +166,7 @@
             symbols: '൦ ൧ ൨ ൩ ൪ ൫ ൬ ൭ ൮ ൯ ഓം ഽ । ॥'.split(' '),
             other: '        റ'.split(' '),
             accent: ["", ""],
-			candra: [''],
+            candra: [''],
             combo_accent: ["", "", "", ""]
         },
 
@@ -183,7 +183,7 @@
             symbols: '୦ ୧ ୨ ୩ ୪ ୫ ୬ ୭ ୮ ୯ ଓଂ ଽ । ॥'.split(' '),
             other: '    ଡ ଢ  ଯ '.split(' '),
             accent: ["", ""],
-			candra: [''],
+            candra: [''],
             combo_accent: ["", "", "", ""]
         },
 
@@ -198,11 +198,11 @@
             other_marks: 'ம்’ : '.split(' '),
             virama: ['்'],
             consonants: 'க க² க³ க⁴ ங ச ச² ஜ ச ஞ ட ட² ட³ ட⁴ ண த த² த³ த⁴ ந ப ப² ப³ ப⁴ ம ய ர ல வ ஶ ஷ ஸ ஹ ள க்ஷ ஜ்ஞ'.split(' '),
-/*            symbols: '௦ ௧ ௨ ௩ ௪ ௫ ௬ ௭ ௮ ௯ ஓம்ʼ ऽ । ॥'.split(' '), */
+/*          symbols: '௦ ௧ ௨ ௩ ௪ ௫ ௬ ௭ ௮ ௯ ஓம்ʼ ऽ । ॥'.split(' '), */
             symbols: '0 1 2 3 4 5 6 7 8 9 ௐ ऽ । ॥'.split(' '),
             other: '        ற'.split(' '),
             accent: ["", ""],
-			candra: [''],
+            candra: [''],
             combo_accent: ["", "", "", ""]
         },
 
@@ -219,7 +219,7 @@
             symbols: '౦ ౧ ౨ ౩ ౪ ౫ ౬ ౭ ౮ ౯ ఓం ఽ । ॥'.split(' '),
             other: '        ఱ'.split(' '),
             accent: ["", ""],
-			candra: [''],
+            candra: [''],
             combo_accent: ["", "", "", ""]
         },
 
@@ -233,7 +233,7 @@
             virama: [''],
             consonants: 'k kh g gh ṅ c ch j jh ñ ṭ ṭh ḍ ḍh ṇ t th d dh n p ph b bh m y r l v ś ṣ s h ḻ kṣ jñ'.split(' '),
             symbols: "0 1 2 3 4 5 6 7 8 9 oṃ ' । ॥".split(' '),
-			candra: ['̆']
+            candra: ['̆']
         },
 
         /* ITRANS
@@ -268,7 +268,7 @@
             virama: [''],
             consonants: 'k kh g gh G c ch j jh J T Th D Dh N t th d dh n p ph b bh m y r l v z S s h L kS jJ'.split(' '),
             symbols: "0 1 2 3 4 5 6 7 8 9 OM ' | ||".split(' '),
-			candra: ['']
+            candra: ['']
         },
 
         /* National Library at Kolkata
@@ -290,7 +290,7 @@
             virama: [''],
             consonants: 'k K g G N c C j J Y w W q Q R t T d D n p P b B m y r l v S z s h L kz jY'.split(' '),
             symbols: "0 1 2 3 4 5 6 7 8 9 oM ' . ..".split(' '),
-			candra: ['̆']
+            candra: ['̆']
         },
 
         /* Velthuis
@@ -303,7 +303,7 @@
             virama: [''],
             consonants: 'k kh g gh "n c ch j jh ~n .t .th .d .d .n t th d dh n p ph b bh m y r l v ~s .s s h L k.s j~n'.split(' '),
             symbols: "0 1 2 3 4 5 6 7 8 9 o.m ' | ||".split(' '),
-			candra: ['̆']
+            candra: ['̆']
         },
 
         /* WX
@@ -316,7 +316,7 @@
             virama: [''],
             consonants: 'k K g G f c C j J F t T d D N w W x X n p P b B m y r l v S R s h  kR jF'.split(' '),
             symbols: "0 1 2 3 4 5 6 7 8 9 oM ' | ||".split(' '),
-			candra: ['̆']
+            candra: ['̆']
         }
     },
 
@@ -746,9 +746,9 @@
         if (to === 'devanagari' && options.enableSanskritVedicAccents === true) {
             //alldata = alldata.replace('\\"', "\u1CDA").replace('"', "\u1CDA").replace('&quot;', "\u1CDA");
             alldata = alldata.replace(/\\?"/g, "\u1CDA");
-	} else if (! (to === "itrans" || to == "iast")) {
+  } else if (! (to === "itrans" || to == "iast")) {
             alldata = alldata.replace(/\\?"/g, "");
-	}
+  }
 
         // Enable Tamil Accents Support
         if (to == 'tamil' && options.enableTamilPronounciation == true) {
