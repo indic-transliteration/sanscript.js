5e9233cc8aa1ea44073bbff343c2d0290611b1be
bkis <kiss@mailbox.org>
2018-08-31 13:22:55 +0200

    added prototype support for ISO 15919 (WIP)

--- sanscript/sanscript.js
+++ sanscript/sanscript.js
@@ -206,6 +206,19 @@
             "symbols": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "oṃ", "'", "।", "॥"]
         },
 
+        /* ISO 15919
+         * --------------------------------------------------
+         * International standard for romanization by the International Organization for Standardization
+         * for "Transliteration of Devanagari and related Indic scripts into Latin characters"
+         */
+        "iso": {
+            "vowels": ["a", "ā", "i", "ī", "u", "ū", "r̥", "r̥̄", "l̥", "l̥̄", "ē", "e", "ai", "ō", "o", "au"],
+            "other_marks": ["ṁ", "ḥ", "m̐"],
+            "virama": [""],
+            "consonants": ["k", "kh", "g", "gh", "ṅ", "c", "ch", "j", "jh", "ñ", "ṭ", "ṭh", "ḍ", "ḍh", "ṇ", "t", "th", "d", "dh", "n", "p", "ph", "b", "bh", "m", "y", "r", "l", "v", "ś", "ṣ", "s", "h", "ḻ", "kṣ", "jñ"],
+            "symbols": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "oṃ", "’", "।", "॥"]
+        },
+
         /* ITRANS
          * ------
          * One of the first romanization schemes -- and one of the most
