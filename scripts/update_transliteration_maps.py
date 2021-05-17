54e9bc7425a0e34279ca686146c071a01b66250f
vvasuki <vishvas.vasuki@gmail.com>
2021-05-17 10:07:32 +0530

    Better placeholders

--- scripts/update_transliteration_maps.py
+++ scripts/update_transliteration_maps.py
@@ -1,12 +1,15 @@
 import codecs
 import difflib
 import glob
+import json
 import logging
 import os.path
 import shutil
+from collections import OrderedDict
 from pprint import pprint
 
 import aksharamukha.transliterate
+import regex
 import sys
 from aksharamukha import GeneralMap
 
@@ -25,21 +28,32 @@ def update_maps():
     devanagari_json_path = os.path.join(REPO_BASE, "src/schemes/brahmic/devanagari.json")
     with codecs.open(devanagari_json_path, "r", "utf-8") as f:
         devanagari_json = f.read()
+        dev_charmap = json.loads(devanagari_json)
     for file in files:
         if "devanagari" in file or "_" in file:
             continue
+        dest_script = os.path.basename(file).capitalize().replace(".json", "")
+        if dest_script.lower() not in ["assamese"]:
+            continue
         with codecs.open(file, "r", "utf-8") as f:
             prior_json = f.read()
-        dest_script = os.path.basename(file).capitalize().replace(".json", "")
+            charmap_prior = json.loads(prior_json)
         posterior_json = aksharamukha.transliterate.process(src=GeneralMap.DEVANAGARI, tgt=dest_script, txt=devanagari_json)
 
         if posterior_json != prior_json:
             logging.info("Difference in %s:", file)
-            diff = differ.compare(prior_json, posterior_json)
-            for x in diff:
-                pprint(x)
+            charmap = OrderedDict()
+            for key in dev_charmap.keys():
+                charmap[key] = []
+                for index, x in enumerate(dev_charmap[key]):
+                    if key in charmap_prior and len(charmap_prior[key]) > index and charmap_prior[key][index] == "":
+                        charmap[key].append(x)
+                    else:
+                        charmap[key].append(aksharamukha.transliterate.process(src=GeneralMap.DEVANAGARI, tgt=dest_script, txt=x))
             with codecs.open(file, "w", "utf-8") as f:
-                f.write(posterior_json)
+                json_str = json.dumps(charmap, indent=4, ensure_ascii=False)
+                json_str = regex.sub(r"\",\n +", '", ', json_str)
+                f.write(json_str)
 
 
 if __name__ == '__main__':
